f8dabc3c62b0f0ece0ebd90a92465eb9b59e5452
JoshBenn <sublimeibanez@protonmail.com>
2024-04-03 12:34:21 -0700

    added lines

--- bytecrypt_ui/src/App.tsx
+++ bytecrypt_ui/src/App.tsx
@@ -11,15 +11,19 @@ export default function App() {
             <div className="mx-10 my-4 header flex justify-between">
                 <Header />
             </div>
+            <hr />
 
             <div className="content mx-10 self-start">
                 <Content />
             </div>
 
-            <div className="footer mx-10 mt-auto flex mb-4 justify-between">
-                <Footer />
+            <div className="mt-auto">
+                <hr />
+                <div className="footer my-4 mx-10 flex justify-between">
+                    <Footer />
+                </div>
             </div>
         </div>
-    )
+    );
 }
 
