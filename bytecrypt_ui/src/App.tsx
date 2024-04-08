79338b766c83e3c2dc76acde4be2ef0f9e3d8dba
JoshBenn <sublimeibanez@protonmail.com>
2024-04-08 12:26:23 -0700

    Reworked the site to work with mobile

--- bytecrypt_ui/src/App.tsx
+++ bytecrypt_ui/src/App.tsx
@@ -7,23 +7,26 @@ import Footer from "./components/Footer";
 export default function App() {
 
     return (
-        <div className="App flex flex-col justify-between text-3xl text-white h-screen">
-            <div className="mx-10 my-4 header flex justify-between">
-                <Header />
-            </div>
-            <hr />
-
-            <div className="content mx-10 self-start">
-                <Content />
-            </div>
-
-            <div className="mt-auto">
+        <>
+            <div className="text-white fixed top-0 w-full flex flex-col">
+                <div className="my-4 header flex sm:flex-row flex-col sm:justify-between justify-center mx-auto sm:mx-10">
+                    <Header />
+                </div>
                 <hr />
-                <div className="footer my-4 mx-10 flex justify-between">
-                    <Footer />
+            </div>
+            <div className="App flex flex-col justify-between text-3xl text-white h-screen pt-24">
+                <div className="content mx-auto self-start my-10 flex flex-col md:flex-row justify-center">
+                    <Content />
+                </div>
+
+                <div className="mt-auto">
+                    <hr />
+                    <div className="footer my-4 mx-10 flex sm:flex-row flex-col sm:justify-between">
+                        <Footer />
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
 
