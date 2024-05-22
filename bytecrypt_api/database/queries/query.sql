f69494b1428d002216e76ba997cf703031a837de
JoshBenn <sublimeibanez@protonmail.com>
2024-05-21 17:17:33 -0700

    Expanding sql for administrators

--- bytecrypt_api/database/queries/query.sql
+++ bytecrypt_api/database/queries/query.sql
@@ -2,25 +2,35 @@
 SELECT * FROM subscriptions;
 
 -- name: GetSubscriptionEmail :one
-SELECT * FROM subscriptions
-WHERE email = $1;
+SELECT * FROM subscriptions WHERE email = $1;
 
 -- name: GetSubscriptionId :one
-SELECT * FROM subscriptions
-WHERE id = $1;
+SELECT * FROM subscriptions WHERE id = $1;
 
 -- name: AddSubscription :one
-INSERT INTO subscriptions (
-    email, name
-) VALUES (
-    $1, $2
-)
-RETURNING *;
+INSERT INTO
+    subscriptions (email, name)
+VALUES ($1, $2) RETURNING *;
 
 -- name: DeleteSubscriptionId :exec
-DELETE FROM subscriptions 
-WHERE id = $1;
+DELETE FROM subscriptions WHERE id = $1;
 
 -- name: DeleteSubscriptionEmail :exec
-DELETE FROM subscriptions 
-WHERE email = $1;
+DELETE FROM subscriptions WHERE email = $1;
+
+-- name: AddAdmin :one
+INSERT INTO
+    administrator (
+        id,
+        email,
+        name,
+        username,
+        password,
+        role
+    )
+VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;
+
+-- name: DeleteAdmin :exec
+DELETE FROM administrator WHERE id = $1
+
+-- name: ChangeAdminRole :one
--- bytecrypt_api/database/schemas/schema.sql
+++ bytecrypt_api/database/schemas/schema.sql
@@ -1,6 +1,20 @@
 CREATE TABLE IF NOT EXISTS subscriptions (
-    id bigserial,
+    id bigserial PRIMARY KEY,
     email text NOT NULL UNIQUE,
     name text NOT NULL,
-    PRIMARY KEY(id)
+)
+
+CREATE TABLE IF NOT EXISTS role (
+    id SERIAL PRIMARY KEY,
+    title text NOT NULL UNIQUE,
+)
+
+CREATE TABLE IF NOT EXISTS administrator (
+    id UUID PRIMARY KEY,
+    email TEXT NOT NULL UNIQUE,
+    name TEXT NOT NULL,
+    username TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    role INT NOT NULL,
+    FOREIGN KEY (role) REFERENCES role(id)
 )
